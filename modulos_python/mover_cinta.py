3a2b53d26b9386195b477614ea50522243120356
AliAbdelhamid <aabdsal@upv.edu.es>
2026-05-28 00:01:07 +0200

    separat escalado i posicion de pictogramas y fotos personalizadas

--- modulos_python/mover_cinta.py
+++ modulos_python/mover_cinta.py
@@ -99,14 +99,26 @@ def mover_cinta_cuadro_acabada():
 
         ruta_rel = var.cola_etiquetas.get()
         ruta_abs = (Path(__file__).resolve().parents[1] / "web" / ruta_rel).as_posix()
-        pegatina = sim.crear_pegatina_obj(RDK, ruta_abs, var.frame_cinta_etiqueta, 0.0001)
+
+        x : int
+        y : int
+
+        if ruta_rel.startswith("uploads/"):
+            pegatina = sim.crear_pegatina_obj(RDK, ruta_abs, var.frame_cinta_etiqueta, 0.0002)
+            x = -50
+            y = -30
+
+        elif ruta_rel.startswith("images/"):
+            pegatina = sim.crear_pegatina_obj(RDK, ruta_abs, var.frame_cinta_etiqueta, 0.0001)
+            x = 120
+            y = 190
 
         if pegatina.Valid():
-            pegatina.setParentStatic(cuadro_obj)
-            pose_etiqueta = (robomath.transl(70, 130, 100))    
+            pegatina.setParentStatic(cuadro_obj)  
+            pose_etiqueta = (robomath.transl(x, y, 100))  
             pegatina.setPose(pose_etiqueta)
             pegatina.setName("pegatina" + cuadro_obj.Name())
-
+        
         robomath.pause(3.0)
 
     _mover_cinta(var.cinta_etiqueta, "SensorFinalEtiqueta", "FrameEtiqueta")
